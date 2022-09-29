7efe5f2333b4f3d57dcea064d8bb0c66209483e5
Miguel Paredes Casado <mparedes@c3r1s5.42madrid.com>
2022-09-29 18:57:46 +0200

    iiii

--- cursus/libft/ft_substr.c
+++ cursus/libft/ft_substr.c
@@ -5,11 +5,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	char 	*dest;
-
+//char a[]="lorem ipsum dolor sit amet";
+//char b[]="lorem ipsu";
+//char c[] = "";
 	i = 0;
-	dest = (char *)malloc(sizeof(char) * len);
-	if (!dest)
+	if (!s)//protegiendo el malloc
 		return (NULL);
+	dest = (char *)malloc(sizeof(char) * len);
 	while (i < len && s[start + i] != '\0')
 	{
 		dest[i] = s[start + i];
