9d33e5e5ad53b636c27d460892479a66c65a9b56
Miguel Paredes Casado <mparedes@c3r1s5.42madrid.com>
2022-10-17 18:16:34 +0200

    auto

---
+++ cursus/libft/ft_split.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mparedes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 18:13:56 by mparedes          #+#    #+#             */
+/*   Updated: 2022/10/17 18:13:57 by mparedes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+/*
+	s: La string a separar
+	c: El carácter delimitador
+
+	return: array de nuevas strings resultante de la separación
+	NULL si falla la reserva de memoria
+*/
+char	**ft_split(char const *s, char c)
+{
+
+}
