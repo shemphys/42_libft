a0adacd976dd8965017aa570e3abc07d4dbe0d12
Miguel Paredes Casado <mparedes@c3r1s5.42madrid.com>
2022-10-17 17:44:48 +0200

    fixed and ready

--- cursus/libft/ft_substr.c
+++ cursus/libft/ft_substr.c
@@ -7,13 +7,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char 	*dest;
 
 	i = 0;
+	if (!s)
+		return (NULL);
 	if (len <= (size_t)ft_strlen(s))
 		dest = malloc(sizeof(char) * (len + 1));
 	else
 		dest = malloc(sizeof(char) * (ft_strlen(s) + 1));
 	if (!dest)
 		return (NULL);
-	while (i < len && s[start + i] != '\0' && (size_t)start <= ft_strlen(s))
+	while (i < len && (size_t)start + i < ft_strlen(s))
 	{
 		dest[i] = s[start + i];
 		i++;
@@ -21,3 +23,29 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	dest[i] = '\0';
 	return (dest);
 }
+/*char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*cpy;
+	size_t			i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	if (!s)
+		return (0);
+	if (len <= (size_t)ft_strlen(s))
+		cpy = (char *)malloc(sizeof(char) * (len + 1));
+	else
+		cpy = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!cpy)
+		return (0);
+	while (s[j] != '\0' && i < len)
+	{
+		if (j >= start)
+			cpy[i++] = s[j];
+		j++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}*/
+
