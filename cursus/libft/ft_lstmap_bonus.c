6300853aadd9a65e18c4b0cb23a288ce94a8dba1
shemphys <mykjestic@gmail.com>
2023-02-27 22:51:12 +0100

    last one----->

--- cursus/libft/ft_lstmap_bonus.c
+++ cursus/libft/ft_lstmap_bonus.c
@@ -2,5 +2,22 @@
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    
+	t_list	*n_item;
+	t_list	*n_lst;
+
+	n_lst = NULL;
+	if (!f || !del)
+		return (NULL);
+	while (lst)
+	{
+		n_item = ft_lstnew(f(lst->content));
+		if (n_item == NULL)
+		{
+			ft_lstclear(&n_lst, n_item);
+			return (NULL);
+		}
+		ft_lstadd_back(&n_lst, n_item)
+		lst = lst->next;
+	}
+	return (n_lst);
 }
