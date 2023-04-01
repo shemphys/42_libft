83c7b6086805669c85bfa1f829780baee822233d
Miguel Paredes Casado <mparedes@c2r19s6.42madrid.com>
2023-04-01 20:39:59 +0200

    parriba42

--- cursus/ft_printf/ft_printf.c
+++ cursus/ft_printf/ft_printf.c
@@ -12,7 +12,29 @@
 
 #include "ft_printf.h"
 
-int	ft_printf(char const *str, ...)
+int	ft_printf(char const *str, ...)//primer argumento debe ser una cadena de caracteres
 {
-	
+	/* NO HACE FALTA
+	char	recoge_args;//tomará los valores de los argumentos según iteremos
+	*/
+	va_list archivo;//nodo tipo va_list donde recogeremos todos los argumentos
+	int		x;
+
+	x = 0;
+	//creamos la lista enlazada
+	va_start(archivo, str);
+
+	while (!str)//el primer
+	{
+		if (str == '%' && !(str++ == 'c' || str++ == 's' || str++ == 'p' 
+		|| str++ == 'i' || str++ == 'd' || str++ == 'u' || str++ == 'x' 
+		|| str++ == 'X' || str++ == '%'))
+			ft_error()
+			ft_menu(str++);
+		str++;
+	}
+	return (1);	//TIENE QUE DEVOLVER UN INT pero aún no sé qué int devolverá haha
+				//¿quizás la suma de todos los caracteres?
+				//Quizás haya que implementar un salto de línea al terminar de imprimir por pantalla.
+				//ese \n también habrá que contabilizarlo como un caracter más. ¿No?
 }
